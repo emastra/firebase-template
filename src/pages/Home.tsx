148a39416964e2487990a1dd53595ad7d7237fd5
emastra <emiliano.mastra@gmail.com>
2024-01-08 16:00:42 +0100

    use Link

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,24 @@
 import styles from './Home.module.scss';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
         <main className={styles.container}>
             <div className={styles.loginButtonWrap}>
-                <button className={styles.loginButton}>Login</button>
+                {/* <button className={styles.loginButton}>Login</button> */}
+                <Link to={'/login'} className={styles.loginButton}>
+                    Login or Register
+                </Link>
             </div>
             <div className={styles.contentWrap}>
-                <div className={styles.box}>Try Firestore</div>
-                <div className={styles.box}>Try Storage</div>
+                {/* <div className={styles.box}>Try Firestore</div>
+                <div className={styles.box}>Try Storage</div> */}
+                <Link to={'/try-firestore'} className={styles.box}>
+                    Try Firestore
+                </Link>
+                <Link to={'/try-storage'} className={styles.box}>
+                    Try Storage
+                </Link>
             </div>
         </main>
     );
